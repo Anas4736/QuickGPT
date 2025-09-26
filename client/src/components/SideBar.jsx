d92c57d73f136f8aebd183de97a20a3aef26d243
1671301009 <mohdanas47369@gmail.com>
2025-09-27 00:44:20 +0530

    commits

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -103,7 +103,7 @@ const SideBar = () => {
           <img src={assets.user_icon} className="w-7 rounded-full" alt="" />
           <p className="flex-1 text-sm dark:text-primary truncate">{user ? user.name : "Login your account"}</p>
           {user && <img src={assets.logout_icon} className="h-5 cursor-pointer hidden not-dark:invert group-hover:block"/>}
-        </div>
+        </div><div></div>
 
     </div>
   );
